d1a26a9def5071f9e97438e0774d83d3991baa3c
unknown <I202116803@cibertec.edu.pe>
2023-06-21 19:54:08 -0500

    creando repositorio

---
+++ src/main/java/cibertec/edu/Cl3TurpoApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl3TurpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl3TurpoApplication.class, args);
+	}
+
+}
